872dfcaf9d6e89c35e4b0602f41080af954f7d66
Igor Zupunski <ipatheword@live.com>
2023-08-14 12:20:12 +0200

    finished

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "../../redux/counterSlice";
+import "./home-styles.scss";
 
 const Home = () => {
-    return(
-        <div>
+    const dispatch = useDispatch();
+    const counter = useSelector(state => state.counter);
 
+    return(
+        <div className="counter">
+            <h1 className="counter__title">
+                Counter App
+            </h1>
+            <p className="counter__counting">
+                Count: {counter}
+            </p>
+            <div className="counter__button-flex">
+                <button onClick={() => dispatch(increment())} className="counter__button">
+                    Increment
+                </button>
+                <button onClick={() => dispatch(decrement())} className="counter__button">
+                    Decrement
+                </button>
+            </div>
+            
         </div>
     );
 };
